d13b14355d9ee619daa1bb8130414aad746b3e74
Quentin Stoeckel <chteuchteu@gmail.com>
2016-10-08 14:42:53 +0200

    Inject stylesheets & scripts inside document iframes

--- data/js/injector.js
+++ data/js/injector.js
@@ -35,32 +35,50 @@
                 ]
             };
         },
-        injectScript: function(src) {
-            var s = document.createElement('script');
+        injectScript: function(src, target) {
+            var s = target.createElement('script');
             s.src = src;
-            (document.head || document.documentElement).appendChild(s);
+            (target.head || target.documentElement).appendChild(s);
         },
-        injectStylesheet: function(src) {
-            var s = document.createElement('link');
+        injectStylesheet: function(src, target) {
+            var s = target.createElement('link');
             s.rel = 'stylesheet';
             s.href = src;
             s.media = 'all';
             s.type = 'text/css';
-            document.head.appendChild(s);
+            target.head.appendChild(s);
         },
-        injectAll: function() {
+        injectAll: function(target) {
+            if (target == undefined) {
+                console.error('Could not inject dependencies into target.');
+                return;
+            }
+
             var deps = this.getDeps();
             var that = this;
 
             // Inject scripts
             deps.js.forEach(function(uri) {
-                that.injectScript(MaterialNetgearGenie.getDepURI(uri));
+                that.injectScript(MaterialNetgearGenie.getDepURI(uri), target);
             });
 
             // Inject stylesheets
             deps.css.forEach(function(uri) {
-                that.injectStylesheet(MaterialNetgearGenie.getDepURI(uri));
+                that.injectStylesheet(MaterialNetgearGenie.getDepURI(uri), target);
             });
+        },
+        waitFor: function(something, onceReady) {
+            var timeout = 100,
+                t = 0;
+            var i = setInterval(function() {
+                if (something()) {
+                    clearInterval(i);
+                    onceReady();
+                }
+
+                if (t++ > timeout)
+                    clearInterval(i);
+            }, 200);
         }
     };
 
@@ -72,6 +90,19 @@
     // Append Material-Netgear-Genie meta tag to head
     Injector.addMeta(Injector.metaName, true);
 
-    // Inject dependencies
-    Injector.injectAll();
+    // Inject dependencies in main frame
+    Injector.injectAll(document);
+
+    // Inject dependencies in all iframes
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i=0, l=iframes.length; i<l; i++) {
+        var iframe = iframes[i];
+
+        // Mutable variable access precautions
+        iframe.onload = (function (iframe) {
+            return function () {
+                Injector.injectAll(iframe.contentWindow.document);
+            }
+        })(iframe);
+    }
 })();
