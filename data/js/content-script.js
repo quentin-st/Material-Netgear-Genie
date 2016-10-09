439b76cb566b8c4b726de1a1540d21c8ee0d1bb4
Quentin Stoeckel <chteuchteu@gmail.com>
2016-10-09 16:49:42 +0200

    Introduce page-specific styling

--- data/js/content-script.js
+++ data/js/content-script.js
@@ -1,7 +1,12 @@
 /**
  * Material-Netgear-Genie
- * Main script - injected by injector.js
+ * Main script
  */
 (function() {
+    // Check if self is in iframe
+    if (window.self != window.top)
+        document.body.setAttribute('data-iframe', true);
 
+    // Append pathname to body for styling purpose
+    document.body.setAttribute('data-pathname', window.location.pathname);
 })();
